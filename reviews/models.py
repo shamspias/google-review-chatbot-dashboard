64bee490daeea496116459ed0585af337fa11c38
shamspias <shamsuddinpias0@gmail.com>
2023-03-25 22:50:44 +0600

    feat model reviews

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    """
+    Model for Review
+    """
+    author = models.CharField(max_length=255)
+    review_text = models.TextField()
+    response = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author}: {self.review_text}'
