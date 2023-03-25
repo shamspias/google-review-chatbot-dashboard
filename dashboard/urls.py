c42bb95a4906114b7fd10d10bdb727dd4d1594d6
shamspias <shamsuddinpias0@gmail.com>
2023-03-25 23:47:17 +0600

    fix: swagger

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import re_path as url
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Reviews API",
+        default_version='v1',
+        description="API for managing reviews and responses",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny, ],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('reviews.urls')),
+
+    # DRF_YASG URLs
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
