d1aadd6ea71892564a21b8c86b975f71eb270648
shamspias <shamsuddinpias0@gmail.com>
2023-03-25 23:03:50 +0600

    fix: database

--- dashboard/settings/local.py
+++ dashboard/settings/local.py
@@ -2,3 +2,11 @@ from .common import *
 
 DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+# Use SQLite for local development
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
--- dashboard/settings/production.py
+++ dashboard/settings/production.py
@@ -1,4 +1,10 @@
+import dj_database_url
 from .common import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['your_production_domain.com']
+
+# Use PostgreSQL for production
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DJANGO_DATABASE_URL'))
+}
