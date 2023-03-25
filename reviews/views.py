7f7a23b6914f6adc978116e5415e263a5da68932
shamspias <shamsuddinpias0@gmail.com>
2023-03-25 22:51:05 +0600

    feat views

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Review
+from .serializers import ReviewSerializer
 
-# Create your views here.
+
+class ReviewListCreate(generics.ListCreateAPIView):
+    """
+    List all reviews, or create a new review.
+    """
+    queryset = Review.objects.all().order_by('-created_at')
+    serializer_class = ReviewSerializer
