229f61f9979038f248f790eafe718a96d4db09d8
shamspias <shamsuddinpias0@gmail.com>
2023-03-25 22:50:54 +0600

    feat model admin reviews

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    """
+    Admin for Review model
+    """
+    list_display = ('author', 'review_text', 'response', 'created_at')
+    search_fields = ('author', 'review_text', 'response')
+    list_filter = ('created_at',)
+
+
+admin.site.register(Review, ReviewAdmin)
